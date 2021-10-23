13f6880e92658a572640396978c5a82e671b4e45
Pablo Poggio <pablopoggiog@gmail.com>
2021-10-23 20:09:28 -0300

    Show button to connect wallet if there's none connected

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
+import { useState } from "react";
 import styled from "styled-components";
+import { useWallet } from "src/hooks";
+import { Button } from "src/components";
 
 const App = () => {
-  return <Container>Hey there, this is my own empty (for now) React app for the Buildspace COURSE (kidding, "hackathon" 😜 lol), deployed through GH Actions to GH Pages</Container>;
+  const { currentAccount, connectWallet, interactWithContract } = useWallet();
+
+  return (
+    <Container>
+      <Title> Let the games begin!</Title>
+      {!currentAccount && (
+        <Button onClick={connectWallet}>Connect your wallet!</Button>
+      )}
+    </Container>
+  );
 };
 
 export default App;
 
 const Container = styled.div`
   text-align: center;
+  padding: 1em;
+`;
+
+const Title = styled.h1`
+  text-align: center;
+  font-size: 1em;
   padding: 1em;
 `;

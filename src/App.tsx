84d0e4791cbe0e8fe24b0f8008eb255ce4e51918
Pablo Poggio <pablopoggiog@gmail.com>
2021-10-23 19:17:31 -0300

    Add GH Actions and deploy to GH Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import "./App.css";
 import styled from "styled-components";
 
 const App = () => {
-  return <Container></Container>;
+  return <Container>Hey there, this is my own empty (for now) React app for the Buildspace COURSE (kidding, "hackathon" 😜 lol), deployed through GH Actions to GH Pages</Container>;
 };
 
 export default App;
 
 const Container = styled.div`
   text-align: center;
+  padding: 1em;
 `;

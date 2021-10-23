938072770a9ad4e54bae6cf933e74d92f53947ea
Pablo Poggio <pablopoggiog@gmail.com>
2021-10-23 20:21:26 -0300

    Tweak the Button component

--- src/components/Button/index.ts
+++ src/components/Button/index.ts
@@ -1,14 +1,23 @@
 import styled from "styled-components";
 
-export const Button = styled.button`
-  border-radius: 5px;
-  background-color: #63b5cf;
+interface ButtonProps {
+  borderRadius?: string;
+}
+
+export const Button = styled.button<ButtonProps>`
+  border-radius: ${({ borderRadius }) => borderRadius ?? "5px"};
   padding: 10px;
-  color: white;
   cursor: pointer;
   transition: 0.6s;
+  text-align: center;
+  justify-content: center;
+
   &:hover {
     background-color: #3ca2c3;
     transform: scale(1.01);
+  }
+
+  &:focus {
+    outline: 0;
   }
 `;

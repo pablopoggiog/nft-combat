ab22116fe935ddcaef96de40eff17749ca98fda1
Pablo Poggio <pablopoggiog@gmail.com>
2021-10-23 19:02:55 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import styled from "styled-components";
+
+const App = () => {
+  return <Container></Container>;
+};
+
+export default App;
+
+const Container = styled.div`
+  text-align: center;
+`;

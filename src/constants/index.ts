f9a555b4f385a5e34919e1a9afa69e9e3c75616d
Pablo Poggio <pablopoggiog@gmail.com>
2021-10-24 18:16:26 -0300

    Update contract

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,3 @@
 export const OPENSEA_LINK =
   "https://testnets.opensea.io/collection/";
-export const CONTRACT_ADDRESS = "0xF4493Fd72e31d3A1285e49B6250d51CEd20bb856";
+export const CONTRACT_ADDRESS = "0x24AFB7cF27AD7F183580e4423AC60Fff454650eB";

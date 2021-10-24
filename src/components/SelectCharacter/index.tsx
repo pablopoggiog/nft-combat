1a39916b31cb945854515b0db99b6db1a4959335
Pablo Poggio <pablopoggiog@gmail.com>
2021-10-24 17:15:30 -0300

    Polish styles in SelectCharacter and add spinner for when the user mints a new NFT

--- src/components/SelectCharacter/index.tsx
+++ src/components/SelectCharacter/index.tsx
@@ -1,4 +1,5 @@
 import { useContract } from "src/hooks";
+import { Button, Spinner } from "src/components";
 import {
   Container,
   Title,
@@ -8,32 +9,41 @@ import {
   CharacterHp,
   CharacterMaxHp,
   CharacterAttackDamage,
-  CharacterImage,
 } from "./styles";
 
 export const SelectCharacter = () => {
-  const { characters } = useContract();
+  const { characters, mintNft, isLoading } = useContract();
 
   return (
     <Container>
       <Title>Choose your hero and mint it</Title>
       <CharactersContainer>
         {!!characters.length &&
-          characters.map(({ name, hp, maxHp, imageURI, attackDamage }) => (
-            <Character>
-              <CharacterName>{name}</CharacterName>
-              <CharacterImage src={imageURI} />
-              <CharacterHp>
-                Health Points: <strong>{String(hp)}</strong>
-              </CharacterHp>
-              <CharacterMaxHp>
-                Max Health Points: <strong>{String(maxHp)}</strong>
-              </CharacterMaxHp>
-              <CharacterAttackDamage>
-                Attack Damage: <strong>{String(attackDamage)}</strong>
-              </CharacterAttackDamage>
-            </Character>
-          ))}
+          characters.map(
+            ({ index, name, hp, maxHp, imageURI, attackDamage }) => (
+              <Character key={name} backgroundImage={String(imageURI)}>
+                {isLoading ? (
+                  <Spinner />
+                ) : (
+                  <>
+                    <CharacterName>{name}</CharacterName>
+                    <CharacterHp>
+                      Health Points: <strong>{String(hp)}</strong>
+                    </CharacterHp>
+                    <CharacterMaxHp>
+                      Max Health Points: <strong>{String(maxHp)}</strong>
+                    </CharacterMaxHp>
+                    <CharacterAttackDamage>
+                      Attack Damage: <strong>{String(attackDamage)}</strong>
+                    </CharacterAttackDamage>
+                    <Button onClick={() => mintNft(index)}>
+                      Mint a {name}
+                    </Button>
+                  </>
+                )}
+              </Character>
+            )
+          )}
       </CharactersContainer>
     </Container>
   );
--- src/components/SelectCharacter/styles.ts
+++ src/components/SelectCharacter/styles.ts
@@ -21,13 +21,32 @@ export const CharactersContainer = styled.div`
   }
 `;
 
-export const Character = styled.div`
+export const Character = styled.div<{ backgroundImage: string }>`
   cursor: pointer;
   border: 1px solid ${({ theme }) => theme.toggleBorder};
   padding: 1em;
   box-shadow: 1px 1px 20px ${({ theme }) => theme.text};
   border-radius: 8px;
-    width: 100%;
+  width: 100%;
+  position: relative;
+  min-height: 250px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  &::before {
+    content: "";
+    background-image: ${({ backgroundImage }) => `url(${backgroundImage})`};
+    background-position: center;
+    background-size: cover;
+    position: absolute;
+    top: 0px;
+    right: 0px;
+    bottom: 0px;
+    left: 0px;
+    transition: 0.5s;
+  border-radius: 8px;
+  }
 
   @media (max-width: 600px) {
     flex-direction: column;
@@ -35,7 +54,11 @@ export const Character = styled.div`
   }
 
   &:hover {
-      background: rgba(255, 255, 255, 0.1);
+    background: rgba(60, 162, 195, 0.2);
+
+    &::before {
+      opacity: 0.2;
+    }
   }
 `;
 
@@ -46,9 +69,3 @@ export const CharacterHp = styled.p``;
 export const CharacterMaxHp = styled.p``;
 
 export const CharacterAttackDamage = styled.p``;
-
-export const CharacterImage = styled.img`
-  height: 100px;
-  border-radius: 8px;
-  box-shadow: 1px 1px 20px ${({ theme }) => theme.text};
-`;
